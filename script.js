c8464d57bdb5750c079d5ceea62dd24952699392
526bmarchese <526bmarchese@frhsd.com>
2024-11-26 11:35:11 -0500

    Day #2: In class (Updated Javascript and added a image)

--- script.js
+++ script.js
@@ -1,47 +1,63 @@
 //GET user Name
 let userName = prompt("what is your name?")
+let responseImg = document.getElementById('response-img')
+let responseText = document.getElementById('response-text')
+let greeting = document.getElementById('greeting')
 
 //Get user Name to Website
-userName ? document.getElementById('greeting').innerText=`Hello, ${userName}` : document.getElementById('greeting'). innerText='Hello Stranger'
+userName
+    ? (greeting.innerText = `Hello, ${userName}!`)
+    : (greeting.innerText = 'Hello Stranger!')
 
-
-function shakeMagic8Ball(){
+function shakeMagic8Ball() {
     //Get User Question
-let userQuestion = 'Will I adopt a new puppy soon?';
+    let userQuestion = prompt('What do you want to ask the 8 Ball?')
 
-console.log(`${userName} has asked - ${userQuestion}`)
+    //DO NOT TOUCH THIS CODE!
+    let randomNumber = Math.floor(Math.random() * 8);
 
-//DO NOT TOUCH THIS CODE!
-let randomNumber = Math.floor(Math.random() * 8);
+    let eightBall = '';
+    let imageChoice = ''
 
-let eightBall = '';
+    switch (randomNumber) {
+        case 0:
+            eightBall = 'It is certain.';
+            imageChoice = 'https://picsum.photos/id/684/600/400'
+            break;
+        case 1:
+            eightBall = 'It is decidely so.';
+                        imageChoice = 'https://picsum.photos/id/684/600/400'
+            break;
+        case 2:
+            eightBall = 'Reply hazy try again.';
+                        imageChoice = 'image3.jpg'
+            break;
+        case 3:
+            eightBall = 'Cannot predict now.';
+                        imageChoice = 'https://picsum.photos/id/684/600/400'
+            break;
+        case 4:
+            eightBall = 'Do not count on it.';
+                        imageChoice = 'https://picsum.photos/id/684/600/400'
+            break;
+        case 5:
+            eightBall = 'My sources say no.';
+                        imageChoice = 'https://picsum.photos/id/684/600/400'
+            break;
+        case 6:
+            eightBall = 'Outlook not so good.';
+                        imageChoice = 'https://picsum.photos/id/684/600/400'
+            break;
+        case 7:
+            eightBall = 'Signs point to yes.';
+                        imageChoice = 'https://picsum.photos/id/684/600/400'
+            break;
+    }
 
-switch (randomNumber) {
-    case 0:
-        eightBall = 'It is certain.';
-        break;
-    case 1:
-        eightBall = 'It is decidely so.';
-        break;
-    case 2:
-        eightBall = 'Reply hazy try again.';
-        break;
-    case 3:
-        eightBall = 'Cannot predict now.';
-        break;
-    case 4:
-        eightBall = 'Do not count on it.';
-        break;
-    case 5:
-        eightBall = 'My sources say no.';
-        break;
-    case 6:
-        eightBall = 'Outlook not so good.';
-        break;
-    case 7:
-        eightBall = 'Signs point to yes.';
-        break;
-}
+    console.log(`The Magic 8 Ball says ${eightBall}`)
 
-console.log(`The Magic 8 Ball says ${eightBall}`)
+    responseText.innerText = `${userName} has asked ${userQuestion} to the almighty 8 Ball. The 8 Ball has replied ${eightBall}!`
+
+    responseImg.src = imageChoice
+
 }
